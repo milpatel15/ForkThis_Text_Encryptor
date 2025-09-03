1067ba02ca887eaa5a4ceaee700896334d496bc6
Milan Patel <milanpatel1911.mp@gmail.com>
2025-09-04 02:35:14 +0530

    Add Logic file.

---
+++ logic.py
@@ -0,0 +1,21 @@
+from cryptography.fernet import Fernet, InvalidToken
+
+def generate_ket_str() :
+    return(Fernet.generate_key().decode())
+
+def encrypt_message(message: str, key: str) -> str :
+    try:
+        f = Fernet(key.encode())
+        encrypted_message = f.encrypt(message.encode())
+        return encrypted_message.decode()
+    except (ValueError, TypeError):
+        return "ERROR: Invalid Key for Encryption."
+    
+def decrypt_message(encrypted_message: str, key: str) -> str:
+    
+    try:
+        f = Fernet(key.encode())
+        decrypted_message = f.decrypt(encrypted_message.encode())
+        return decrypted_message.decode()
+    except (InvalidToken, TypeError, ValueError):
+        return "ERROR: Decryption Failed. Invalid Key or Message."
